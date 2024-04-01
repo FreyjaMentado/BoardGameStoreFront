2782a000f53e17b2166c19cf5fe33cd0c5daccfb
austinrobichaux18 <54606588+austinrobichaux18@users.noreply.github.com>
2024-04-01 14:23:23 -0500

    Add ReadCsv

--- CardImporter/CardImporter/Main.cs
+++ CardImporter/CardImporter/Main.cs
@@ -1,12 +1,31 @@
-﻿namespace CardImporter;
+﻿using CardImporter.Models;
+using CardImporter.Services;
+using CsvHelper;
+using System.Globalization;
+
+namespace CardImporter;
 
 public static class Main
 {
     public static void Initialize()
     {
-        var contents = File.ReadAllText("Sample_TCG_Import.csv");
+        var models = ReadCsv();
 
+    }
 
-        //Write code in this file 
+    private static List<TcgModel> ReadCsv()
+    {
+        //This pathing dir thing might not work in deployed versions 
+        var fileName = "Sample_TCG_Import.csv";
+        var path = Path.Combine(Environment.CurrentDirectory, @"..\\..\\..\\", fileName);
+
+        using (TextReader txt = new StreamReader(path))
+        {
+            using (var csv = new CsvReader(txt, CultureInfo.CurrentCulture))
+            {
+                _ = csv.Context.RegisterClassMap<ModelClassMap>();
+                return csv.GetRecords<TcgModel>().ToList();
+            }
+        }
     }
 }
--- CardImporter/CardImporter/Models/TcgModel.cs
+++ CardImporter/CardImporter/Models/TcgModel.cs
@@ -1,6 +1,16 @@
 ﻿namespace CardImporter.Models;
-internal class TcgModel
+public class TcgModel
 {
-    //    Quantity,Name,Simple Name, Set, Card Number,Set Code, Printing, Condition, Language, Rarity, Product ID,SKU
-
+    public int Quantity { get; set; }
+    public string Name { get; set; }
+    public string SimpleName { get; set; }
+    public string Set { get; set; }
+    public int CardNumber { get; set; }
+    public string SetCode { get; set; }
+    public string Printing { get; set; }
+    public string Condition { get; set; }
+    public string Language { get; set; }
+    public string Rarity { get; set; }
+    public int ProductID { get; set; }
+    public int SKU { get; set; }
 }
