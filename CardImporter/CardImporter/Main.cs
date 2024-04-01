789469ca09c669eade4f8d2c849464006031df2a
austinrobichaux18 <54606588+austinrobichaux18@users.noreply.github.com>
2024-04-01 14:09:40 -0500

    add TcgModel base

--- CardImporter/CardImporter/Main.cs
+++ CardImporter/CardImporter/Main.cs
@@ -4,6 +4,9 @@ public static class Main
 {
     public static void Initialize()
     {
+        var contents = File.ReadAllText("Sample_TCG_Import.csv");
+
+
         //Write code in this file 
     }
 }
---
+++ CardImporter/CardImporter/Models/TcgModel.cs
@@ -0,0 +1,6 @@
+﻿namespace CardImporter.Models;
+internal class TcgModel
+{
+    //    Quantity,Name,Simple Name, Set, Card Number,Set Code, Printing, Condition, Language, Rarity, Product ID,SKU
+
+}
