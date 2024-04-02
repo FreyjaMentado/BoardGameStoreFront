31da728346b5c2d2c7b870f9e627b16753dab299
Freyja Shelton <freyjamentado@gmail.com>
2024-04-02 10:18:42 -0400

    match scryfall model to scryfall, add mvp doc

---
+++ CardImporter/CardImporter/Mariposa MVP.md
@@ -0,0 +1,178 @@
+# Customer Site Pages
+
+**Home Page** 
+	**Search bar at top**
+
+**Board Game To Sell**
+	**List Page**
+	**Details Page** 
+		Add to cart
+		Wishlist Button
+
+**Cards To Sell**
+	**List Page**
+	**Details Page** 
+		Add to cart
+		Wishlist Button
+
+User Page
+	Wishlist Section
+		Ordered by sale things on top
+		Ability to remove items from list
+		Ability to opt in or out of email notifications when item on sale
+		Navigate to Details Page
+		Add to cart
+		link to share wishlist with other people
+
+**Cart Page (User Item Reservations)**
+	See Items Reserved
+	Price...
+	See Date They marked as reserved
+	Optional enter date/time you plan to pick up bundle 
+		If entered, EOD after this, auto expires if not picked up
+	See date their reservation expires (2days?)
+
+**Bottom of site links**
+	About Us
+	Discord
+	Instagram
+	Facebook
+	Contact us
+	Exchange and Return Policy
+
+# In-Store Worker Site
+
+**View Orders Page**
+	See: Email address, Name, Estimated Arrival Time, Items in bundle, 
+	If employee should bundle prep or not (result of grief detection)
+	Customer Bought Bundle (remove bundle from db)
+	Customer Called in and said they dont want anymore -> expire reservation
+		Bundles do not push to order history directly, only what they actually bought 
+			e.g. they want 123, they show up, only want 12, buy 12, 123 does not push to order history for customer. only 12. 
+
+# Admin Site
+**Manage Inventory Page**
+	**Add new products**
+
+# Sql Tables
+**Index**
+	Everything gets :
+	created/createdby and modified/modifiedby
+	Id
+	Name
+	Description
+	ThumbnailImage
+	DisplayImage
+	List of images
+	SellPrice
+	WholesalePrice (Purchase price)
+	Sale%
+	SaleFlat
+	SalePrice
+	Stock
+	
+**Cards**
+	everything that we get from tcg/scryfall
+**Games**
+	PlayerMinCount
+	PlayerMaxCount
+	GameTypes (list of GameType)
+	AgeRangeMin
+	AverageGameLength
+	GameThemes (List of GameTheme)
+	
+**Users** 
+	Id
+	ThirdPartyId (Guid of user acc mgmt software)
+	IsActive
+	FirstName
+	LastName
+	Date of birth
+	Email
+	
+**Wishlist**
+	Id
+	UserId FK
+	ProductType (value from ProductTypes) FK
+	ProductId FK
+
+**Order**
+	Id
+	UserId FK
+	OrderStatusId FK (value from OrderStatuses) 
+
+**Cart**  
+	 Id
+	UserId FK
+	OrderId FK
+	ProductType (value from ProductTypes) FK
+	ProductId FK
+
+**Image**
+	Id
+	FK
+	Type
+	Binary
+	FKTableName
+
+**UserPermissions**
+	Id
+	UserId
+	PermissionId
+### List Tables
+**GameTypes / Enum?**
+	Id
+	Name
+**GameTheme / Enum?**
+	Id
+	Name
+**OrderStatus / Enum?**
+	Id
+	Name
+	Values: Purchased, ReadyForPickup, PickedUp
+**ProductType / Enum?**
+	Id
+	Name
+### Static Enum-esque object 
+**Permissions**
+	Id (Guid/int/string)
+	Name
+	Description
+# Controllers
+- Master
+	- Get Many (Home page / mgmt bulk search page cross-object search)
+- Card
+	- Post Many (Importer)
+	- Post Single (Mgmt page creation)
+	- Patch Single (Stock, sale, etc changes)
+	- Patch Many (bulk sale changes)
+	- Get Many 
+	- Get Single
+- Game
+	- Post Many (Importer)
+	- Post Single (Mgmt page creation)
+	- Patch Single (Stock, sale, etc changes)
+	- Patch Many (bulk sale changes)
+	- Get Many 
+	- Get Single
+- User
+	- Post Single (Admin Create / User Acc Info from third party manager)
+	- Possible splitting ^ to 2 endpoints
+	- Patch Single (Change perms, first/last/dob/email/etc)
+- Wishlist
+	- Post Single (if none for user, auto create, this takes one new product id/type at a time)
+	- Delete Single (Remove Single Item From Wishlist)
+- Order
+	- Patch Status 
+- Cart
+	- Post Single Item 
+	- Delete Single Item
+	- Purchase (Auto Creates Order once paid for)
+- Image
+	- Get
+	- Post
+	- Delete
+- List (Controller for all list tables)
+	- Post Single
+	- Patch Single 
+	- Delete Single
--- CardImporter/CardImporter/Models/Scry_PostCard_Response.cs
+++ CardImporter/CardImporter/Models/Scry_PostCard_Response.cs
@@ -1,4 +1,6 @@
-﻿namespace CardImporter.Models;
+﻿using Newtonsoft.Json;
+
+namespace CardImporter.Models;
 
 public class Scry_PostCard_Response
 {
@@ -6,131 +8,324 @@ public class Scry_PostCard_Response
 }
 public class Scry_Card
 {
+    [JsonProperty("object")]
     public string Object { get; set; }
+
+    [JsonProperty("id")]
     public string Id { get; set; }
+
+    [JsonProperty("oracle_id")]
     public string OracleId { get; set; }
+
+    [JsonProperty("multiverse_ids")]
     public List<int> MultiverseIds { get; set; }
-    public int MtgoId { get; set; }
-    public int MtgoFoilId { get; set; }
+
+    [JsonProperty("tcgplayer_id")]
     public int TcgplayerId { get; set; }
+
+    [JsonProperty("cardmarket_id")]
     public int CardmarketId { get; set; }
+
+    [JsonProperty("name")]
     public string Name { get; set; }
+
+    [JsonProperty("lang")]
     public string Lang { get; set; }
+
+    [JsonProperty("released_at")]
     public string ReleasedAt { get; set; }
+
+    [JsonProperty("uri")]
     public string Uri { get; set; }
+
+    [JsonProperty("scryfall_uri")]
     public string ScryfallUri { get; set; }
+
+    [JsonProperty("layout")]
     public string Layout { get; set; }
+
+    [JsonProperty("highres_image")]
     public bool HighresImage { get; set; }
+
+    [JsonProperty("image_status")]
     public string ImageStatus { get; set; }
+
+    [JsonProperty("image_uris")]
     public ImageUris ImageUris { get; set; }
+
+    [JsonProperty("mana_cost")]
     public string ManaCost { get; set; }
-    public decimal Cmc { get; set; }
+
+    [JsonProperty("cmc")]
+    public double Cmc { get; set; }
+
+    [JsonProperty("type_line")]
     public string TypeLine { get; set; }
+
+    [JsonProperty("oracle_text")]
     public string OracleText { get; set; }
-    public string Power { get; set; }
-    public string Toughness { get; set; }
+
+    [JsonProperty("colors")]
     public List<object> Colors { get; set; }
-    public List<object> ColorIdentity { get; set; }
+
+    [JsonProperty("color_identity")]
+    public List<string> ColorIdentity { get; set; }
+
+    [JsonProperty("keywords")]
     public List<object> Keywords { get; set; }
-    public Legalities Legalities { get; set; }
-    public List<string> Games { get; set; }
-    public bool Reserved { get; set; }
-    public bool Foil { get; set; }
-    public bool Nonfoil { get; set; }
-    public List<string> Finishes { get; set; }
-    public bool Oversized { get; set; }
-    public bool Promo { get; set; }
-    public bool Reprint { get; set; }
-    public bool Variation { get; set; }
-    public string SetId { get; set; }
-    public string Set { get; set; }
-    public string SetName { get; set; }
-    public string SetType { get; set; }
-    public string SetUri { get; set; }
-    public string SetSearchUri { get; set; }
-    public string ScryfallSetUri { get; set; }
-    public string RulingsUri { get; set; }
-    public string PrintsSearchUri { get; set; }
-    public string CollectorNumber { get; set; }
-    public bool Digital { get; set; }
-    public string Rarity { get; set; }
-    public string FlavorText { get; set; }
-    public string CardBackId { get; set; }
-    public string Artist { get; set; }
-    public List<string> ArtistIds { get; set; }
-    public string IllustrationId { get; set; }
-    public string BorderColor { get; set; }
-    public string Frame { get; set; }
-    public string SecurityStamp { get; set; }
-    public bool FullArt { get; set; }
-    public bool Textless { get; set; }
-    public bool Booster { get; set; }
-    public bool StorySpotlight { get; set; }
-    public int EdhrecRank { get; set; }
-    public int PennyRank { get; set; }
-    public Prices Prices { get; set; }
-    public RelatedUris RelatedUris { get; set; }
-    public PurchaseUris PurchaseUris { get; set; }
+
+    [JsonProperty("produced_mana")]
     public List<string> ProducedMana { get; set; }
+
+    [JsonProperty("legalities")]
+    public Legalities Legalities { get; set; }
+
+    [JsonProperty("games")]
+    public List<string> Games { get; set; }
+
+    [JsonProperty("reserved")]
+    public bool Reserved { get; set; }
+
+    [JsonProperty("foil")]
+    public bool Foil { get; set; }
+
+    [JsonProperty("nonfoil")]
+    public bool Nonfoil { get; set; }
+
+    [JsonProperty("finishes")]
+    public List<string> Finishes { get; set; }
+
+    [JsonProperty("oversized")]
+    public bool Oversized { get; set; }
+
+    [JsonProperty("promo")]
+    public bool Promo { get; set; }
+
+    [JsonProperty("reprint")]
+    public bool Reprint { get; set; }
+
+    [JsonProperty("variation")]
+    public bool Variation { get; set; }
+
+    [JsonProperty("set_id")]
+    public string SetId { get; set; }
+
+    [JsonProperty("set")]
+    public string Set { get; set; }
+
+    [JsonProperty("set_name")]
+    public string SetName { get; set; }
+
+    [JsonProperty("set_type")]
+    public string SetType { get; set; }
+
+    [JsonProperty("set_uri")]
+    public string SetUri { get; set; }
+
+    [JsonProperty("set_search_uri")]
+    public string SetSearchUri { get; set; }
+
+    [JsonProperty("scryfall_set_uri")]
+    public string ScryfallSetUri { get; set; }
+
+    [JsonProperty("rulings_uri")]
+    public string RulingsUri { get; set; }
+
+    [JsonProperty("prints_search_uri")]
+    public string PrintsSearchUri { get; set; }
+
+    [JsonProperty("collector_number")]
+    public string CollectorNumber { get; set; }
+
+    [JsonProperty("digital")]
+    public bool Digital { get; set; }
+
+    [JsonProperty("rarity")]
+    public string Rarity { get; set; }
+
+    [JsonProperty("flavor_text")]
+    public string FlavorText { get; set; }
+
+    [JsonProperty("card_back_id")]
+    public string CardBackId { get; set; }
+
+    [JsonProperty("artist")]
+    public string Artist { get; set; }
+
+    [JsonProperty("artist_ids")]
+    public List<string> ArtistIds { get; set; }
+
+    [JsonProperty("illustration_id")]
+    public string IllustrationId { get; set; }
+
+    [JsonProperty("border_color")]
+    public string BorderColor { get; set; }
+
+    [JsonProperty("frame")]
+    public string Frame { get; set; }
+
+    [JsonProperty("security_stamp")]
+    public string SecurityStamp { get; set; }
+
+    [JsonProperty("full_art")]
+    public bool FullArt { get; set; }
+
+    [JsonProperty("textless")]
+    public bool Textless { get; set; }
+
+    [JsonProperty("booster")]
+    public bool Booster { get; set; }
+
+    [JsonProperty("story_spotlight")]
+    public bool StorySpotlight { get; set; }
+
+    [JsonProperty("edhrec_rank")]
+    public int EdhrecRank { get; set; }
+
+    [JsonProperty("prices")]
+    public Prices Prices { get; set; }
+
+    [JsonProperty("related_uris")]
+    public RelatedUris RelatedUris { get; set; }
+
+    [JsonProperty("purchase_uris")]
+    public PurchaseUris PurchaseUris { get; set; }
 }
 
 public class ImageUris
 {
+    [JsonProperty("small")]
     public string Small { get; set; }
+
+    [JsonProperty("normal")]
     public string Normal { get; set; }
+
+    [JsonProperty("large")]
     public string Large { get; set; }
+
+    [JsonProperty("png")]
     public string Png { get; set; }
+
+    [JsonProperty("art_crop")]
     public string ArtCrop { get; set; }
+
+    [JsonProperty("border_crop")]
     public string BorderCrop { get; set; }
 }
 
 public class Legalities
 {
+    [JsonProperty("standard")]
     public string Standard { get; set; }
+
+    [JsonProperty("future")]
     public string Future { get; set; }
+
+    [JsonProperty("historic")]
     public string Historic { get; set; }
+
+    [JsonProperty("timeless")]
     public string Timeless { get; set; }
+
+    [JsonProperty("gladiator")]
     public string Gladiator { get; set; }
+
+    [JsonProperty("pioneer")]
     public string Pioneer { get; set; }
+
+    [JsonProperty("explorer")]
     public string Explorer { get; set; }
+
+    [JsonProperty("modern")]
     public string Modern { get; set; }
+
+    [JsonProperty("legacy")]
     public string Legacy { get; set; }
+
+    [JsonProperty("pauper")]
     public string Pauper { get; set; }
+
+    [JsonProperty("vintage")]
     public string Vintage { get; set; }
+
+    [JsonProperty("penny")]
     public string Penny { get; set; }
+
+    [JsonProperty("commander")]
     public string Commander { get; set; }
+
+    [JsonProperty("oathbreaker")]
     public string Oathbreaker { get; set; }
+
+    [JsonProperty("standardbrawl")]
     public string Standardbrawl { get; set; }
+
+    [JsonProperty("brawl")]
     public string Brawl { get; set; }
+
+    [JsonProperty("alchemy")]
     public string Alchemy { get; set; }
+
+    [JsonProperty("paupercommander")]
     public string Paupercommander { get; set; }
+
+    [JsonProperty("duel")]
     public string Duel { get; set; }
+
+    [JsonProperty("oldschool")]
     public string Oldschool { get; set; }
+
+    [JsonProperty("premodern")]
     public string Premodern { get; set; }
+
+    [JsonProperty("predh")]
     public string Predh { get; set; }
 }
 
 public class Prices
 {
+    [JsonProperty("usd")]
     public string Usd { get; set; }
-    public string UsdFoil { get; set; }
+
+    [JsonProperty("usd_foil")]
+    public object UsdFoil { get; set; }
+
+    [JsonProperty("usd_etched")]
     public object UsdEtched { get; set; }
+
+    [JsonProperty("eur")]
     public string Eur { get; set; }
-    public string EurFoil { get; set; }
-    public string Tix { get; set; }
+
+    [JsonProperty("eur_foil")]
+    public object EurFoil { get; set; }
+
+    [JsonProperty("tix")]
+    public object Tix { get; set; }
 }
 
 public class PurchaseUris
 {
+    [JsonProperty("tcgplayer")]
     public string Tcgplayer { get; set; }
+
+    [JsonProperty("cardmarket")]
     public string Cardmarket { get; set; }
+
+    [JsonProperty("cardhoarder")]
     public string Cardhoarder { get; set; }
 }
 
 public class RelatedUris
 {
+    [JsonProperty("gatherer")]
     public string Gatherer { get; set; }
+
+    [JsonProperty("tcgplayer_infinite_articles")]
     public string TcgplayerInfiniteArticles { get; set; }
+
+    [JsonProperty("tcgplayer_infinite_decks")]
     public string TcgplayerInfiniteDecks { get; set; }
+
+    [JsonProperty("edhrec")]
     public string Edhrec { get; set; }
 }
