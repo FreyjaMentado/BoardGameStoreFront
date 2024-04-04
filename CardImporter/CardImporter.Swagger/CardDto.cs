fe7c73d47a6e99fd1a33ec759b31c5e4b01aa11f
austinrobichaux18 <54606588+austinrobichaux18@users.noreply.github.com>
2024-04-04 14:46:04 -0500

    remove card importer

--- CardImporter/CardImporter.Swagger/CardDto.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace CardImporter.Swagger;
-public class CardDto
-{
-    //This class will come from API eventually. Make it clean here first. 
-}
--- CardImporter/CardImporter/Models/Scry_PostCard_Request.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CardImporter.Models;
-internal class Scry_PostCard_Request
-{
-    public Scry_PostCard_Request() => identifiers = new List<ScryFall_CardIdentifier_Request>();
-    public List<ScryFall_CardIdentifier_Request> identifiers { get; set; }
-}
-public record ScryFall_CardIdentifier_Request(string Name, string Set);
